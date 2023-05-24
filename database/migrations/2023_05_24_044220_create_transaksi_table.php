87c3d8347738e5e19ac0ee34928448558f30993d
Julfikar <julfikarfirdaus.jf@gmail.com>
2023-05-24 11:56:21 +0700

    migration transaksi, rekening, kategori

---
+++ database/migrations/2023_05_24_044220_create_transaksi_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransaksiTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transaksi', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('kategori_id');
+            $table->unsignedInteger('rekening_id');
+            $table->integer('jumlah');
+            $table->text('keterangan');
+            $table->dateTime('tanggal');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaksi');
+    }
+}
